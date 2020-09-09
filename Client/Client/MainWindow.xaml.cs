9c61102833037035bb36b104f08b512acb57614d
Andrej997 <andrej.km997@gmail.com>
2020-09-09 19:23:44 +0200

    Start of project
    WPF Client
    WCF Service

---
+++ Client/Client/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Client.ServiceReference1;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly LogInClient client;
+        public MainWindow()
+        {
+            if (client == null)
+                client = new LogInClient();
+
+            InitializeComponent();
+        }
+
+        private void btLogin_Click(object sender, RoutedEventArgs e)
+        {
+            client.LogIn(tbUsername.Text, tbPassword.Text);
+        }
+    }
+}
---
+++ Service/Common/LogIn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Common
+{
+    public class LogIn : ILogIn
+    {
+        int ILogIn.LogIn(string username, string password)
+        {
+            Console.WriteLine(username + " " + password);
+            return 1;
+        }
+    }
+}
